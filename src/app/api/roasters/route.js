4a155404c6589054e947134c4333d2173a1cdb19
Your name here <elia.orsini@hotmail.com>
2024-07-02 11:14:54 +0100

    fix: replace worker url with env variable

--- src/app/api/roasters/route.js
+++ src/app/api/roasters/route.js
@@ -1,6 +1,6 @@
 export async function GET() {
   const data = await fetch(
-    `https://notion-cloudflare-worker.elia-orsini.workers.dev/v1/table/${process.env.ROASTERS_NOTION_URL}`,
+    `${process.env.CLOUDFLARE_WORKER}/v1/table/${process.env.ROASTERS_NOTION_URL}`,
     { next: { revalidate: 30 } }
   ).then((res) => res.json());
 
