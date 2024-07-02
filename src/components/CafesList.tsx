353dec6fb455babd55043de90de7ffd964b8f437
Your name here <elia.orsini@hotmail.com>
2024-07-02 09:12:59 +0100

    fix: sort cafes by city name

--- src/components/CafesList.tsx
+++ src/components/CafesList.tsx
@@ -14,9 +14,16 @@ const CafesList: React.FC<{ cafes: ICafe[]; roasters: IRoaster[] }> = ({
     );
   }
 
+  const sortedCafes = cafes.sort((a, b) => {
+    if (a.city < b.city) return -1;
+    if (a.city > b.city) return 1;
+
+    return 0;
+  });
+
   return (
     <div className="z-10 -mt-6 sm:-mt-20 flex-col-3 sm:w-3/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 m-10 sm:m-auto gap-4 sm:gap-10">
-      {cafes.map((cafe) => {
+      {sortedCafes.map((cafe) => {
         return (
           <SingleCafe
             key={`cafeCard-${cafe.id}`}
